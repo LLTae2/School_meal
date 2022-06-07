160d16d7bb113d8ff3dd43a3c3f98415a6f20201
taehyun7262 <taehyun5820@gmail.com>
2022-06-07 11:27:08 +0900

    학교 급식 api 가져옴

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  // sc_meal = open.neis.go.kr/hub/mealServiceDietInfo?KEY="1bd1716ede994c7f9aa616a2d4a87c88"&Type=json&pIndex=1&pSize=10&ATPT_OFCDC_SC_CODE=C10&SD_SCHUL_CODE=7150658;
+  // console.log(sc_meal);
+
+ 
+  console.log('')
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
+//https://open.neis.go.kr/hub/mealServiceDietInfo?ATPT_OFCDC_SC_CODE=C10&SD_SCHUL_CODE=7150658&MLSV_YMD=20220715
